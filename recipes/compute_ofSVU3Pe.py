976f11bd11ad0687b2e1fde6ed594869540bc111
admin <admin>
2022-11-22 16:31:35 +0100

    Recipe CCFRAUD_VIZ.compute_ofSVU3Pe saved through notebook notebook editor for compute_ofSVU3Pe

--- recipes/compute_ofSVU3Pe.py
+++ recipes/compute_ofSVU3Pe.py
@@ -1,11 +1,12 @@
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
-#%matplotlib inline
+%matplotlib inline
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
@@ -24,13 +25,13 @@ df_by_deciles = transactions_known_scored.groupby(by='decile').agg({'authorized_
                                                                     'proba_1':'count'}).rename(columns=col_names)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df_by_deciles['validation_ratio'] = df_by_deciles.sum_true/df_by_deciles.count_pred
+df_by_deciles['total'] = df_by_deciles.sum_true+df_by_deciles.count_pred
+df_by_deciles['validation_ratio'] = df_by_deciles.sum_true/df_by_deciles.total
 df_by_deciles.sort_index(ascending=True, inplace=True)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df_by_deciles.head()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+sns.set_style("ticks",{'axes.grid' : True})
+colors = ['#24b1d1', '#ae24d1']
 labels = list(df_by_deciles.index)
 sum_true = list(df_by_deciles.sum_true)
 count_pred = list(df_by_deciles.count_pred)
@@ -38,22 +39,57 @@ validation_ratio = list(df_by_deciles.validation_ratio)
 width = 2/3 # the width of the bars
 
 fig, ax1 = plt.subplots(figsize=(12, 6))
-ax1.bar(labels, count_pred, width, label='count_pred')
-ax1.bar(labels, sum_true, width, bottom=count_pred, label='sum_true')
+ax1.bar(labels, count_pred, width, label='count_pred', color=colors[0])
+ax1.bar(labels, sum_true, width, bottom=count_pred, label='sum_true', color=colors[1])
 color = 'b'
 ax1.set_ylabel('Volume', color=color)
 ax1.tick_params('y', colors=color)
-ax1.legend()
+ax1.legend(loc='upper left')
 
+# Set an offset that is used to bump the label up a bit above the bar.
+y_offset = 5
+# Add total value to each bar.
+for i, total in enumerate(df_by_deciles.total):
+    ax1.text(df_by_deciles.total.index[i], total + y_offset, '{:,}'.format(round(total)), ha='center', weight='bold')
+
+# For each patch (basically each rectangle within the bar), add a label.
+for bar in ax1.patches:
+    ax1.text(
+      # Put the text in the middle of each bar. get_x returns the start
+      # so we add half the width to get to the middle.
+      bar.get_x() + bar.get_width() / 2,
+      # Vertically, add the height of the bar to the start of the bar,
+      # along with the offset.
+      bar.get_height() + bar.get_y() / 2,
+      # This is actual value we'll show.
+      #round(bar.get_height()),
+      '{:,}'.format(round(bar.get_height())),
+      # Center the labels and style them a bit.
+      ha='center',
+      color='w',
+      weight='bold',
+      size=10
+    )
+    
+    
 ax2 = ax1.twinx()
 color = 'r'
-ax2.plot(labels, validation_ratio, color=color)
-ax2.set_ylabel('Percentage', color=color)
+ax2.plot(labels, validation_ratio, label='True ratio', color=color)
+ax2.set_ylabel('Percentage = sum_true / count_pred', color=color)
 ax2.tick_params('y', colors=color)
+ax2.legend(loc='upper right')
 
-#plt.title('')
+# Add ratio value to each bar.
+for i, ratio in enumerate(validation_ratio):
+    ax2.text(df_by_deciles.validation_ratio.index[i], ratio, '{:,.1%}'.format(ratio), ha='center', weight='bold')
+
+plt.title('Real vs predicted observations by decile (on the validation set)')
 fig.tight_layout()
 plt.show()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+## Vérifier qu'on a les bons volumes (somme notamment) et commencer une review à présenter
+## Dans quelle table le mettre
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Save plot to folder
